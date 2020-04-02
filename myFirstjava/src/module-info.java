38f5003c69c2539c52f58f94aac620ea0dc19e87
brronma <brronma@gmail.com>
2020-04-02 11:36:38 -0400

    commit from biruk

---
+++ myFirstjava/src/module-info.java
@@ -0,0 +1,18 @@
+public class MyClass {
+  public static void main(String[] args)
+  {
+	  int varMyvar=100;
+	  
+	  int secondVarMyvar=varMyvar;
+	  int varMyvar1=50;
+	  
+	  
+			  
+    System.out.println(varMyvar1++);
+    System.out.println(varMyvar1);
+    System.out.println(secondVarMyvar);
+    System.out.println(varMyvar1++);
+    System.out.println(varMyvar1);
+    System.out.println(secondVarMyvar);
+  }
+}
